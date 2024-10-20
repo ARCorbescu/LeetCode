9ae8fad08f75029ff85f6e117d969aadd00cf890
Corbescu <arcorbescu@luxoft.com>
2024-10-20 16:59:17 +0300

    Slight improvement for the .py solution

--- Python/jump_game.py
+++ Python/jump_game.py
@@ -10,10 +10,9 @@ class Solution(object):
         for index in range(n):
             if index > max_next:
                 return False
-            
-            max_next = max(max_next, index + nums[index])
-            
-            if max_next >= n - 1:
-                return True
+            else:
+                max_next = max(max_next, index + nums[index])
+                if max_next >= n - 1:
+                    return True
                 
         return False
